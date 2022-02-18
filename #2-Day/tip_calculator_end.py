36653239d89748554142ea3966f3b4fe641891b4
Darshan P <72539638+1darshanpatil@users.noreply.github.com>
2022-02-18 14:54:02 +0530

    Update tip_calculator_end.py

--- #2-Day/tip_calculator_end.py
+++ #2-Day/tip_calculator_end.py
@@ -3,4 +3,4 @@ bill = int(input('What was the total bill $'))
 tip_percent = int(input("How much tip would you like to give? 10, 12, or 15?"))
 people = int(input("How many people to split the bill?"))
 billed = bill * (1 + tip_percent/100) /people
-print("Each person should pay: $", billed)
+print("Each person should pay: $", round(billed))
