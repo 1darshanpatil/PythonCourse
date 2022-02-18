f40affd19e78c131cdbcc8d7dc4bd58dbe08a4d8
Darshan P <72539638+1darshanpatil@users.noreply.github.com>
2022-02-18 15:42:57 +0530

    Create BMI Calculator.py

---
+++ #2-Day/BMI Calculator.py
@@ -0,0 +1,7 @@
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+w, h = float(weight), float(height)
+#Write your code below this line 👇
+print(int(w/(h**2))) #Remember float1 // float2 = float3 No matter even if you take floor division
