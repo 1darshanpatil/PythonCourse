08324196fff1b00356b60968bb7d4e704e1ed379
Darshan P <72539638+1darshanpatil@users.noreply.github.com>
2022-02-19 10:33:46 +0530

    Update Project_Treasure Island End.py

--- #3-Day/Project_Treasure Island End.py
+++ #3-Day/Project_Treasure Island End.py
@@ -3,11 +3,11 @@ print("Your mission is to find the treasure.")
 if input('Choose direction "left" or "right"? ').lower() == "left":
 	if input("It's a river swim or wait for a boat? ") == "wait":
 		door = input("There are three doors on of Red, Yellow, and Blue, Which to choose? ")
-		if door == "Red":
+		if door.lower() == "red":
 			print("Red means fire it's a room of fire... Game Over!")
-		elif door == "Blue":
+		elif door.lower() == "blue":
 			print("Eaten by a blue giant, Game Over!")
-		elif door == "Yellow":
+		elif door.lower() == "yellow":
 			print("Yellow means treasure, You win!")
 		else:
 			print("You invented your own door, Door of Hell, Game Over!")
@@ -15,9 +15,7 @@ if input('Choose direction "left" or "right"? ').lower() == "left":
 		print("Attacked by trout Game Over.")
 
 else:
-	print("Right is not always right, sometimes left is also right, Game Over!")
-
-	
+	print("Right is not always right, sometimes left is also right, Game Over!")	
 	
 #Map of the treasure ok flow chart
 
