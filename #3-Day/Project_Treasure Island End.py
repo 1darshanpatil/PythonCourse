ae34b2fd70db053dc79475be6cbfedee283010d2
Darshan P <72539638+1darshanpatil@users.noreply.github.com>
2022-02-19 10:30:54 +0530

    Update Project_Treasure Island End.py

--- #3-Day/Project_Treasure Island End.py
+++ #3-Day/Project_Treasure Island End.py
@@ -16,3 +16,11 @@ if input('Choose direction "left" or "right"? ').lower() == "left":
 
 else:
 	print("Right is not always right, sometimes left is also right, Game Over!")
+
+	
+	
+#Map of the treasure ok flow chart
+
+'''
+https://viewer.diagrams.net/?highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+'''
