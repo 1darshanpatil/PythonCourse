f00057aba2800af7ba89640300fc76d59b991a30
Darshan P <72539638+1darshanpatil@users.noreply.github.com>
2022-02-21 19:52:33 +0530

    Add files via upload

---
+++ #17-Day/Menu.py
@@ -0,0 +1,5 @@
+class Menu():
+    menu_ = {"Latte": {"WATER": 200, "MILK": 150, "COFFEE": 24, "MONEY": 25,},
+    "Espresso": {"WATER": 50, "MILK": 0, "COFFEE": 18, "MONEY": 15},
+    "Cappuccino": {"WATER": 250, "MILK": 50, "COFFEE": 24, "MONEY": 30}
+    }
