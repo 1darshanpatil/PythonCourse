765c724b88ba5c29be2439ab571375e565d4b03b
Darshan P <72539638+1darshanpatil@users.noreply.github.com>
2022-02-21 19:53:39 +0530

    Update main.py

--- #17-Day/main.py
+++ #17-Day/main.py
@@ -1,7 +1,10 @@
 import Menu
 W, M, C, COST = 800, 500, 150, 0.0
 class TakingMoney():
-    Drinks = Menu.Menu().menu_
+    Drinks = {"Latte": {"WATER": 200, "MILK": 150, "COFFEE": 24, "MONEY": 25,},
+    "Espresso": {"WATER": 50, "MILK": 0, "COFFEE": 18, "MONEY": 15},
+    "Cappuccino": {"WATER": 250, "MILK": 50, "COFFEE": 24, "MONEY": 30}
+    }
     def __init__(self, drink):
         self.cup = drink
 
