43891b2276fe9cb34c21fdbd813426cdd1b2f13a
Darshan P <72539638+1darshanpatil@users.noreply.github.com>
2022-03-01 13:04:17 +0530

    Update and rename random_direction.py to random_direction_with_random_colors.py

--- #18-Day/random_direction_with_random_colors.py
+++ #18-Day/random_direction_with_random_colors.py
@@ -7,11 +7,13 @@ def r_g_b_color_tuple():
     return color_tuple
 
 direction = [0, 90, 180, 270] #Nort, East, South West
-
+turtle.colormode(255)
+scrn = turtle.Screen()
 
 pen.speed('fastest')
 for x in range(100):
     pen.fd(random.randrange(40, 70))
+    pen.color(r_g_b_color_tuple())
     pen.seth(random.choice(direction))
 
    
